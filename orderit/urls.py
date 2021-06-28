1e19ed7f429d9f2d3a568f3cf9970438a8669f2d
Ajibola Ojo <41059344+jbrit@users.noreply.github.com>
2021-06-28 11:04:42 +0100

    Update urls.py

--- orderit/urls.py
+++ orderit/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    # Auth Views
+    path('api/auth/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
