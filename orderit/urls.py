8ad9b833dcb6990faa6fa1c8b6809e07293b45e3
Elijah Soladoye <shodown96@gmail.com>
2021-07-06 14:11:00 +0100

    authentication-proceses

--- orderit/urls.py
+++ orderit/urls.py
@@ -1,8 +1,6 @@
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt import views as jwt_views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,7 +8,5 @@ urlpatterns = [
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     # Auth Views
-    path('api/auth/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/auth/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('vauth.urls')),
+    path('api/auth/', include('vauth.urls')),
 ]
