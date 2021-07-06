b21d85570094c16cc5a2bb472a41e7c782003d24
Elijah Soladoye <shodown96@gmail.com>
2021-07-06 09:42:48 +0100

    Profile model created

--- vauth/views.py
+++ vauth/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework import permissions
-
+from rest_framework.views import APIView
 from .serializers import RegisterUserSerializer
+from django.contrib.auth import get_user_model
+from .serializers import ChangePasswordSerializer
+User = get_user_model()
+
 
 class RegisterUserView(CreateAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = RegisterUserSerializer
     queryset = ''
+
+
+class ChangePasswordView(CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
