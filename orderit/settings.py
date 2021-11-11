ce8a7f4161da2ff792e9772a54968ac71220cf9f
Elijah Soladoye <shodown96@gmail.com>
2021-11-11 14:24:16 +0100

    heroku

--- orderit/settings.py
+++ orderit/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        # 'HOST': 'db',
         'PORT': 5432,
     }
 }
