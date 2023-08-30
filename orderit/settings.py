96a8e7395e13c9c1aa0c1357b49d1940b3b11be6
Elijah Soladoye <shodown96@gmail.com>
2023-08-30 13:14:53 +0100

    main

--- orderit/settings.py
+++ orderit/settings.py
@@ -111,6 +111,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = '/media/blog/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "vauth.User"
